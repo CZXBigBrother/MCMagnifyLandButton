71952dfdd8155011857f054a16794f2cc8f16d59
marcoChen <chenxingghost@gmail.com>
2017-01-06 13:54:16 +0800

    添加备注

--- MCMagnifyLandButtonExample/MCMagnifyLandButtonExample/MCMagnifyLandButton.h
+++ MCMagnifyLandButtonExample/MCMagnifyLandButtonExample/MCMagnifyLandButton.h
@@ -15,14 +15,44 @@
 @property(assign, nonatomic,readonly) CGFloat boundH;
 @property(assign, nonatomic,readonly) CGFloat boundDW;
 @property(assign, nonatomic,readonly) CGFloat boundDH;
+/*
+ *  清空
+ */
 - (void)clear;
+/*
+ *  拓展右边空间
+ */
 - (void)increaseRight:(CGFloat)width;
+/*
+ *  拓展左边空间
+ */
 - (void)increaseLeft:(CGFloat)width;
+/*
+ *  拓展上层空间
+ */
 - (void)increaseUp:(CGFloat)width;
+/*
+ *  拓展下层空间
+ */
 - (void)increaseDown:(CGFloat)width;
+/*
+ *  向右移动
+ */
 - (void)moveRight:(CGFloat)width;
+/*
+ *  向左移动
+ */
 - (void)moveLeft:(CGFloat)width;
+/*
+ *  向上移动
+ */
 - (void)moveUp:(CGFloat)width;
+/*
+ *  向下移动
+ */
 - (void)moveDown:(CGFloat)width;
+/*
+ *  原始大小翻倍,不包括拓展空间
+ */
 - (void)multiLand:(NSInteger)multi;
 @end
